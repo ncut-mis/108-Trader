e1c5d2af81e9a13b44b10eda4de056ff7f6dab43
Hsin-Ju Teng <dlulu900821@gmail.com>
2022-02-04 15:34:48 +0800

    會員 訂單 1對多關係

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Order extends Model
 {
     use HasFactory;
+    public function members()
+    {
+        return $this->belongsTo(Member::class);
+    }
 }
