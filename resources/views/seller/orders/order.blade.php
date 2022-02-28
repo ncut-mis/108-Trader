42a110b0000518c83bde04cb1d9722c6245b6773
chen shu ting <a.0066715@gmail.com>
2022-02-28 14:02:52 +0800

    訂單管理版面

---
+++ resources/views/seller/orders/order.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts.master')
+
+@section('title','訂單管理')
+
+@section('breadcrumb')
+    <li class="breadcrumb-item" style="color: #4E4F97"><a href="#">首頁</a></li>
+    <li class="breadcrumb-item" style="color: #4E4F97"><a href="#">訂單管理</a></li>
+@endsection
+
+@section('content')
+    <thead>
+    <tr>
+        <th width="35" style="text-align: center">訂單編號</th>
+        <th width="20" style="text-align: center">運送狀態</th>
+        <th width="20" style="text-align: center"></th>
+    </tr>
+    </thead>
+    <tfoot>
+    <tr>
+        <td style="text-align: center"></td>
+        <td></td>
+        <td style="text-align: center"><a href="">修改</a></td>
+    </tr>
+    </tfoot>
+    <tbody>
+@endsection
+
+
+
+
+
+
+
+
