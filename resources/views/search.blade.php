febb19fefa0ff7f452db83b972484e91328e8c6e
chen yun fan <o07080826mina@gmail.com>
2022-03-30 21:57:28 +0800

    刪除註解

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -87,21 +87,6 @@
             @endforeach
         </div>
 
-{{--        @if($products->isEmpty())--}}
-{{--            <p>&nbsp;</p>--}}
-{{--            <p>&nbsp;</p>--}}
-{{--            <p>&nbsp;</p>--}}
-{{--            <p>&nbsp;</p>--}}
-{{--            <div style="margin:0px auto;">--}}
-{{--                <h2>查無結果</h2>--}}
-{{--            </div>--}}
-{{--            <h4>查無結果</h4>--}}
-{{--            <p>&nbsp;</p>--}}
-{{--            <p>&nbsp;</p>--}}
-{{--            <p>&nbsp;</p>--}}
-{{--            <p>&nbsp;</p>--}}
-{{--        @endif--}}
-
         <!-- Load more -->
 {{--        <div class="flex-c-m flex-w w-full p-t-45">--}}
 {{--            <a href="#" class="flex-c-m stext-101 cl5 size-103 bg2 bor1 hov-btn1 p-lr-15 trans-04">--}}
