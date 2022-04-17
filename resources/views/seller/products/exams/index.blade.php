fdbf2b01aa4d284e3a530ab19351f68f2e1161a0
chen shu ting <a.0066715@gmail.com>
2022-04-17 11:13:20 +0800

    所有品質鑑定商品的版面

---
+++ resources/views/seller/products/exams/index.blade.php
@@ -0,0 +1,52 @@
+@extends('seller.layouts.master')
+
+@section('title','品質鑑定商品')
+
+@section('breadcrumb')
+    <li class="breadcrumb-item" style="color: #4E4F97"><a href="#">首頁</a></li>
+    <li class="breadcrumb-item active" style="color: #4E4F97">品質鑑定商品</li>
+@endsection
+
+@section('content')
+    <thead>
+    <tr>
+        <th width="20" >商品名稱</th>
+        <th width="20" >類別</th>
+        <th width="20" >是否通過</th>
+        <th width="20" >是否優良</th>
+        <th width="20" ></th>
+        <th width="20" >鑑定日期</th>
+
+    </tr>
+    </thead>
+
+    @foreach($product as $exam)
+
+        <tfoot>
+        <tbody>
+        <tr>
+            <td>{{$name}}</td>
+            <td>{{$category}}</td>
+
+                @if($exam->pass=='0')
+                <td>未通過</td>
+                @else
+                <td>通過</td>
+                @endif
+
+            @if($exam->perfect=='0')
+                <td>非優良</td>
+            @else
+                <td>優良</td>
+            @endif
+
+            <td ><a href="#" style="color:#4E4F97"></a></td>
+            <td>{{$exam->date}}</td>
+        </tr>
+        </tfoot>
+        </tbody>
+
+    @endforeach
+@endsection
+
+
