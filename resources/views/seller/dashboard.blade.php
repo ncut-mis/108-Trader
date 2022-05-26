1c6cacf6c46318a81c7464a4ccb3539f1b229bcb
chen shu ting <a.0066715@gmail.com>
2022-05-27 03:51:33 +0800

    將圖表註解

--- resources/views/seller/dashboard.blade.php
+++ resources/views/seller/dashboard.blade.php
@@ -85,9 +85,9 @@
             </div>
         </div>
 
-        <div class="col-xl-8 col-lg-7">
-            <div class="card shadow mb-4">
-                <!-- Card Header - Dropdown -->
+        <!-- <div class="col-xl-8 col-lg-7">
+        <div class="card shadow mb-4">
+
                 <div
                     class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
                     <h6 class="m-0 font-weight-bold text-primary">商品排行</h6>
@@ -144,15 +144,15 @@
                 </div>
             </div>
         </div>
-        <!-- Pie Chart -->
+    Pie Chart
         <div class="col-xl-4 col-lg-5">
             <div class="card shadow mb-4">
-                <!-- Card Header - Dropdown -->
+
                 <div
                     class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
                     <h6 class="m-0 font-weight-bold text-primary">Revenue Sources</h6>
                 </div>
-                <!-- Card Body -->
+
                 <div class="card-body">
                     <div class="chart-pie pt-4 pb-2">
                         <canvas id="PieChart">
@@ -215,7 +215,8 @@
                         <span class="mr-2">
                                             <i class="fas fa-circle text-info"></i> Referral
                                         </span>
-                    </div>
+                    </div>-->
+
                 </div>
             </div>
         </div>
