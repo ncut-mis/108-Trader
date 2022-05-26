aead148d52c8a78d00bb5b05436df0990ab63b13
chen shu ting <a.0066715@gmail.com>
2022-05-26 15:09:00 +0800

    賣家商品詳細資料版面

---
+++ resources/views/seller/products/detail.blade.php
@@ -0,0 +1,73 @@
+@extends('seller.layouts.master')
+
+@section('title','詳細資訊')
+
+@section('button')
+    <a href="{{url('/products/create')}}" class="d-none d-sm-inline-block btn btn-sm btn-facebook shadow-sm">新增商品</a>
+@endsection
+
+@section('breadcrumb')
+    <li class="breadcrumb-item" style="color: #4E4F97"><a href="{{route('seller.dashboard')}}">首頁</a></li>
+    <li class="breadcrumb-item active" style="color: #4E4F97">詳細資訊</li>
+@endsection
+
+@section('content')
+    <thead>
+    <tr>
+        <th width="20">商品圖片</th>
+        <th width="20" >商品名稱</th>
+        <th width="20" >類別</th>
+        <th width="20" >價格</th>
+        <th width="20" >庫存</th>
+        <th width="20" >細節說明</th>
+        <th width="20" >上架狀態</th>
+        <th width="20" >操作</th>
+        <th width="20" >申請品質鑑定</th>
+
+    </tr>
+    </thead>
+    @foreach($data as $product)
+        <tfoot>
+        <tbody>
+        <tr>
+            <?php
+            $pic = $product->pictures;
+            ?>
+            <td ><img class="pic" src="{{ asset('img/'.$pic.'') }}"></td>
+            <td >{{$product->name}}</td>
+
+                @foreach($name as $cname)
+                @if($product->category_id==$cname->id)
+                    <td>{{$cname->name}}</td>
+                @endif
+                @endforeach
+
+            <td >{{$product->price}}</td>
+            <td >{{$product->stock}}</td>
+            <td>{{$product->detail}}</td>
+
+            @if($product->status == '1')
+                <td >已上架<br>
+                    <hr class="sidebar-divider d-none d-md-block">
+                    <b><a href="{{route('seller.products.stop', $product->id)}}" style="color:#DC9FB4" onClick="return confirm('確定要下架此商品?')">下架</a></b>
+                </td>
+            @else
+                <td >未上架<br>
+                    <hr class="sidebar-divider d-none d-md-block">
+                    <b><a href="{{route('seller.products.launch', $product->id)}}" style="color:#4E4F97" onClick="return confirm('確定要上架此商品?')">上架</a></b>
+                </td>
+            @endif
+
+            <td >
+                <a href="{{ route('seller.products.edit', $product->id) }}" style="color:#4E4F97">編輯</a><br>
+                <hr class="sidebar-divider d-none d-md-block">
+                <a href="{{ route('seller.products.destroy',  $product->id) }}" style="color:#DC9FB4" onClick="return confirm('確定要刪除此商品?')">刪除</a>
+            </td>
+
+            <td ><a href="{{ route('products.exams.create', $product->id) }}" style="color:#4E4F97">申請</a></td>
+        </tr>
+        </tfoot>
+        </tbody>
+
+    @endforeach
+@endsection
